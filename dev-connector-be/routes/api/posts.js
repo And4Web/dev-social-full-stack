31d44581d3df68af830824ed3b4113228cc37a05
And4Web <anands2807@gmail.com>
2022-09-02 09:03:25 +0530

    like route created.

--- dev-connector-be/routes/api/posts.js
+++ dev-connector-be/routes/api/posts.js
@@ -23,7 +23,10 @@ router.get('/test', (req, res)=>res.json({msg: "posts is working."}))
 // @access Public
 
 router.get('/', (req, res) => {
-  Post.find().sort({date: -1}).then(posts=>res.json(posts)).catch(err=>res.status(404).json({noPostsFound: 'no posts found.'}))
+  Post.find()
+  .sort({date: -1})
+  .then(posts => res.json(posts))
+  .catch(() => res.status(404).json({noPostsFound: 'no posts found.'}))
 })
 
 // @route GET api/posts/:postId
@@ -31,7 +34,9 @@ router.get('/', (req, res) => {
 // @access Public
 
 router.get('/:postId', (req, res)=>{
-  Post.findById(req.params.postId).then(post=>res.json(post)).catch(err=>res.status(404).json({noPostFound: 'no post having this id found.'}))
+  Post.findById(req.params.postId)
+  .then(post => res.json(post))
+  .catch(() => res.status(404).json({noPostFound: 'no post having this id found.'}))
 })
 
 // @route POST api/posts
@@ -74,6 +79,29 @@ router.delete('/:postId', passport.authenticate("jwt", {session: false}), (req, 
       post.remove().then(() => res.json({success: true}));
     })
     .catch(err=> res.staus(400).json({postNotFound: 'no post found.'}))
+  })
+})
+
+// @route POST api/posts/like/:postId
+// @desc  Like post
+// @access Private
+
+router.post('/:postId', passport.authenticate("jwt", {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+  .then(profile => {
+    Post.findById(req.params.postId)
+    .then(post => {
+      
+      //Check if already liked
+      if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+        return res.status(404).json({alreadyLiked: 'Your already lked this post.'})
+      }
+      //Add user to likes array
+      post.likes.unshift({user: req.user.id})
+
+      post.save().then(post => res.json(post))
+    })
+    .catch(() => res.staus(400).json({postNotFound: 'no post found.'}))
   })
 })
 
